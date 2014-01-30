e3419929accbc929a3aafe7f6be26b9538db702e
victor1tnet <truica.victor.alexandru@gmail.com>
2014-01-30 15:46:13 +0100

    more download locations, arrays of files to delete

--- wp_deploy.sh
+++ wp_deploy.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-WPDWD="http://wordpress.org/latest.zip"
+# download list - core wp + plugins
+DWD_WP="http://wordpress.org/latest.zip"
+DWD_BWPUP="http://downloads.wordpress.org/plugin/backwpup.3.1.1.zip"
+DWD_BBQ="http://downloads.wordpress.org/plugin/block-bad-queries.zip"
+DWD_SEO="http://downloads.wordpress.org/plugin/wordpress-seo.1.4.24.zip"
+DWD_GA="http://downloads.wordpress.org/plugin/google-analytics-for-wordpress.4.3.4.zip"
+DWD_SMG="http://downloads.wordpress.org/plugin/google-sitemap-generator.3.3.1.zip"
 
-wget $WPDWD
+# extra links
+WP_SECRETSALT="https://api.wordpress.org/secret-key/1.1/salt/"
+
+# files
+WPZIP="latest.zip"
+
+# array of files to delete
+FILES2DEL=( license.txt readme.html )
+
+#echo ${FILES2DEL[@]}
+
+
+############################################################
+
+# download wordpress
+#wget $DWD_WP
+
+# extract and modify accordingly
+#unzip $WPZIP && rm -rf $WPZIP
+cd wordpress 
+
+# delete files
+rm -rf ${FILES2DEL[@]}
+
+#download secret keys + salt
+#wget $WP_SECRETSALT -O salt
